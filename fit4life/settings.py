82e965e66894ca249a9d26704cb78e87ea15ee88
Houssem Aoun <aounhe@hotmail.com>
2024-03-27 10:53:13 +0000

    deployment

--- fit4life/settings.py
+++ fit4life/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-hnpg6uwpcq+^77ir8+@v+imggt+l!dep_b1t51ugg74$c%ohkm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '8000-aounhe-fit4life-xi2zsq33rey.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = [ 'film4lifms4.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,13 +120,18 @@ WSGI_APPLICATION = 'fit4life.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+     
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
