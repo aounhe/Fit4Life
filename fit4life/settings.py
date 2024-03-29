59126a68b3d67e576295af36537c4333e74e0224
Houssem Aoun <aounhe@hotmail.com>
2024-03-29 11:19:49 +0000

    database name change

--- fit4life/settings.py
+++ fit4life/settings.py
@@ -121,9 +121,9 @@ WSGI_APPLICATION = 'fit4life.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
+if 'DATABASE_URL1' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL1'))
     }
 else:
     DATABASES = {
